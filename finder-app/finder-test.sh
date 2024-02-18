803c2e5a6aefa7e4b5e9aa69aef148142a4d1e3e
Steffen Ronalter <ronalterde@gmail.com>
2024-02-18 18:54:42 +0100

    Adapt paths for assignment 4, part 2

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -5,10 +5,11 @@
 set -e
 set -u
 
+CONF_DIR=/etc/finder-app
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+username=$(cat ${CONF_DIR}/username.txt)
 
 if [ $# -lt 3 ]
 then
@@ -32,7 +33,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat conf/assignment.txt`
+assignment=`cat ${CONF_DIR}/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
@@ -55,10 +56,12 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
+
+echo ${OUTPUTSTRING} > /tmp/assignment4-result.txt
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
